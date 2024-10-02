605582bf50d394f1b92898f2d40fea8fd76b9a6c
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2024-10-02 14:40:02 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,5 +1,19 @@
+#include "vfs.h"
+#include <stdio.h>
+
 void kernel_main() {
-    // OS 초기화
-    // VFS 초기화 시작
     vfs_init();
+
+    file_t* file = vfs_open("hello.txt");
+    if (file != NULL) {
+        char buffer[12];
+        int bytesRead = vfs_read(file, buffer, 11);
+        buffer[bytesRead] = '\0';
+
+        printf("File contents: %s\n", buffer);
+
+        vfs_close(file);
+    } else {
+        printf("File not found.\n");
+    }
 }
